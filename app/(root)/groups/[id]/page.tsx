cebb084282023eede13210a0b7e1d8906cc349df
gowtham20402 <gowthamchandrasekaran01@gmail.com>
2023-08-12 14:40:51 +0530

    error resolved in groups view

--- app/(root)/groups/[id]/page.tsx
+++ app/(root)/groups/[id]/page.tsx
@@ -44,7 +44,7 @@ async function Page({ params }: { params: { id: string } }) {
 
                 {tab.label === "Thoughts" && (
                   <p className='ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2'>
-                    {groupDetails.Thoughts.length}
+                    {groupDetails.thoughts.length}
                   </p>
                 )}
               </TabsTrigger>
